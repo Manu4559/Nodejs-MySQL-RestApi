6b310f92e8309d4181c34fd5879ffadfb7b0d1a2
Manu <76539155+Manu4559@users.noreply.github.com>
2022-12-27 01:14:10 +0100

    Modifcado database.sql

--- db/database.sql
+++ db/database.sql
@@ -2,14 +2,14 @@ CREATE DATABASE IF NOT EXISTS companydb;
 
 USE companydb;
 
-CREATE TABLE employees (
+CREATE TABLE employee (
     id INT(11) NOT NULL AUTO_INCREMENT,
     name VARCHAR(45) DEFAULT NULL,
     salary INT(5) DEFAULT NULL,
     PRIMARY KEY (ID)
 )
 
-INSERT INTO employees VALUES
+INSERT INTO employee VALUES
  (1, 'Joe', 1000),
  (2, 'Henry', 2000),
  (3, 'Sam', 2500),
